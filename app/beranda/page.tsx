d4bded328a3532adc832a43b5a661bf2f41f7d58
marcosihombing72 <marcosihombing72@gmail.com>
2025-08-05 09:44:35 +0700

    update

--- app/beranda/page.tsx
+++ app/beranda/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 import Button from "@/components/Button";
